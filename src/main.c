aff757b41c49da9a4c8ca76b6c25062029f9b922
WyvernAllow <bloodflame2535@gmail.com>
2024-09-15 15:45:15 +0200

    single line viewing

--- src/main.c
+++ src/main.c
@@ -4,8 +4,12 @@
 #include <string.h>
 
 #include "platform.h"
+#include "keys.h"
 
-#define WP_CTRL_KEY(k) ((k) & 0x1F)
+struct wp_row {
+    size_t size;
+    char *chars;
+};
 
 struct wp_state {
     bool is_running;
@@ -15,6 +19,9 @@ struct wp_state {
 
     size_t screen_cols;
     size_t screen_rows;
+
+    size_t num_rows;
+    struct wp_row row;
 };
 
 int wp_state_init(struct wp_state *state) {
@@ -27,35 +34,82 @@ int wp_state_init(struct wp_state *state) {
         return -1;
     }
 
+    state->num_rows = 0;
+
     return 0;
+}
+
+int wp_open(struct wp_state *state, const char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if(!fp) {
+        fprintf(stderr, "Failed to open file. %s\n", filename);
+        return -1;
+    }
+
+    char *line = NULL;
+    size_t line_cap = 0;
+    size_t line_len;
+
+    line_len = getline(&line, &line_cap, fp);
+    if(line_len != -1) {
+        while (line_len > 0 && (line[line_len - 1] == '\n' || line[line_len - 1] == '\r')) {
+            line_len--;
+        }
+
+        state->row.size = line_len;
+        state->row.chars = malloc(line_len + 1);
+        memcpy(state->row.chars, line, line_len);
+        state->row.chars[line_len] = '\0';
+        state->num_rows = 1;
+    }
+
+    free(line);
+    fclose(fp);
+
+    return 0;
+}
+
+void wp_draw_welcome_message(struct wp_state *state) {
+    char *welcome_message = "wraithpad editor -- version 0.1.0";
+    int welcome_len = strlen(welcome_message);
+
+    int padding = (state->screen_cols - welcome_len) / 2;
+    if (padding > 0) {
+        fputs("~", stdout);
+        padding--;
+    }
+
+    while (padding--) {
+        fputs(" ", stdout);
+    }
+
+    fputs(welcome_message, stdout);
+}
+
+void wp_draw_row(struct wp_state *state, size_t y) {
+    if (y >= state->num_rows) {
+        fputs("~", stdout);
+    } else {
+        size_t len = state->row.size;
+        if (len > state->screen_cols) {
+            len = state->screen_cols;
+        }
+        fwrite(state->row.chars, sizeof(char), len, stdout);
+    }
 }
 
 void wp_draw(struct wp_state *state) {
     fputs("\x1b[?25l", stdout);
     fputs("\x1b[H", stdout);
 
-    for(int y = 0; y < state->screen_rows; y++) {
-        if (y == state->screen_rows / 2) {
-            char *welcome_message = "wraithpad editor -- version 0.1.0";
-            int welcome_len = strlen(welcome_message);
-
-            int padding = (state->screen_cols - welcome_len) / 2;
-            if (padding) {
-                fputs("~", stdout);
-                padding--;
-            }
-
-            while (padding--) {
-                fputs(" ", stdout);
-            }
-
-            fputs(welcome_message, stdout);
+    for(size_t y = 0; y < state->screen_rows; y++) {
+        if (state->num_rows == 0 && y == state->screen_rows / 2) {
+            wp_draw_welcome_message(state);
         } else {
-            fputs("~", stdout);
+            wp_draw_row(state, y);
         }
 
         fputs("\x1b[K", stdout);
-
         if (y < state->screen_rows - 1) {
             fputs("\r\n", stdout);
         }
@@ -65,68 +119,6 @@ void wp_draw(struct wp_state *state) {
 
     fputs("\x1b[?25h", stdout);
     fflush(stdout);
-}
-
-enum wp_key_type {
-    WP_KEY_ARROW_UP,
-    WP_KEY_ARROW_DOWN,
-    WP_KEY_ARROW_LEFT,
-    WP_KEY_ARROW_RIGHT,
-
-    WP_KEY_ARROW_CHAR,
-};
-
-struct wp_key {
-    enum wp_key_type type;
-    char character;
-};
-
-struct wp_key wp_make_key(enum wp_key_type type) {
-    struct wp_key key = {
-        .type = type,
-        .character = '\0'
-    };
-
-    return key;
-}
-
-struct wp_key wp_make_char(char c) {
-    struct wp_key key = {
-        .type = WP_KEY_ARROW_CHAR,
-        .character = c
-    };
-
-    return key;
-}
-
-struct wp_key wp_read_key() {
-    char c;
-    fread(&c, sizeof(c), 1, stdin);
-
-    if(c == '\x1b') {
-        char seq[3];
-
-        if(fread(&seq[0], sizeof(char), 1, stdin) != 1) {
-            return wp_make_char('\x1b');
-        }
-
-        if(fread(&seq[1], sizeof(char), 1, stdin) != 1) {
-            return wp_make_char('\x1b');
-        }
-
-        if(seq[0] == '[') {
-            switch (seq[1]) {
-                case 'A': return wp_make_key(WP_KEY_ARROW_UP);
-                case 'B': return wp_make_key(WP_KEY_ARROW_DOWN);
-                case 'C': return wp_make_key(WP_KEY_ARROW_RIGHT);
-                case 'D': return wp_make_key(WP_KEY_ARROW_LEFT);
-            }
-        }
-
-        return wp_make_char('\x1b');
-    }
-
-    return wp_make_char(c);
 }
 
 void wp_update(struct wp_state *state) {
@@ -153,8 +145,8 @@ void wp_update(struct wp_state *state) {
             state->cursor_x++;
         }
         break;
-    case WP_KEY_ARROW_CHAR:
-        if(key.character == WP_CTRL_KEY('q')) {
+    case WP_KEY_CHAR:
+        if(wp_ctrl_key(key, 'q')) {
             state->is_running = false;
         }
         break;
@@ -165,23 +157,26 @@ int main(int argc, char **argv) {
     if(wp_enable_raw_mode() == -1) {
         fputs("\x1b[2J", stdout);
         fprintf(stderr, "Failed to enable raw mode");
+        wp_disable_raw_mode();
         return EXIT_FAILURE;
     }
 
     struct wp_state state;
     wp_state_init(&state);
 
+    if (argc >= 2) {
+        if(wp_open(&state, argv[1]) == -1) {
+            wp_disable_raw_mode();
+            return EXIT_FAILURE;
+        }
+    }
+
     while(state.is_running) {
         wp_draw(&state);
         wp_update(&state);
     }
 
-    if(wp_disable_raw_mode() == -1) {
-        fputs("\x1b[2J", stdout);
-        fprintf(stderr, "Failed to disable raw mode");
-        return EXIT_FAILURE;
-    }
-
+    wp_disable_raw_mode();
     fputs("\x1b[2J", stdout);
     return EXIT_SUCCESS;
 }
